3c49a791efab585dae479f49e580e90be636afc4
Stixxxxx <Stixxx@inbox.ru>
2017-04-26 21:41:19 +0300

    В первый коммит новый класс LOG не добавился в репу, щас прикрутил, Комичу

---
+++ Log.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CallBook
+{
+    class Log
+    {
+
+        public static void addToLog(string text)
+        {
+            DateTime localDate = DateTime.Now;
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"Log.txt", true))
+            {
+                file.WriteLine(localDate + " | " + text);
+            }
+
+        }
+             
+    }
+    
+}
